c69219d31944e894aabe5438849208db4b8e795c
phattin <tin220905@gmail.com>
2024-10-11 12:28:47 +0700

    login_register_page

--- main.js
+++ main.js
@@ -11,45 +11,43 @@ searchbtn.addEventListener("click", () => {
 });
 
 // ---------------------------------- hero slide show ---------------------------------
-const listImg = document.querySelector('.list-hero')
-const imgs = document.querySelectorAll('.hero-img')
-const nextHero = document.querySelector('.hero-btn-next')
-const prevHero = document.querySelector('.hero-btn-prev')
-const dotList = document.querySelector('#dots-list')
-let dots = document.querySelectorAll('.dot-item')
-const lengthImgs = imgs.length
-let currentImg = 0
-let widthImg = imgs[0].offsetWidth
+const listImg = document.querySelector(".list-hero");
+const imgs = document.querySelectorAll(".hero-img");
+const nextHero = document.querySelector(".hero-btn-next");
+const prevHero = document.querySelector(".hero-btn-prev");
+const dotList = document.querySelector("#dots-list");
+let dots = document.querySelectorAll(".dot-item");
+const lengthImgs = imgs.length;
+let currentImg = 0;
+let widthImg = imgs[0].offsetWidth;
 slideShownext = () => {
-  if(currentImg == lengthImgs - 1){
+  if (currentImg == lengthImgs - 1) {
     currentImg = 0;
+  } else {
+    currentImg++;
   }
-  else {
-    currentImg++
-  }
-  listImg.style.transform = `translateX(${widthImg * -1 * currentImg}px)`
-  document.querySelector('.dot-item.active').classList.remove('active')
-  dots[currentImg].classList.add('active')
-}
-let slideShowauto = setInterval(slideShownext, 4000)
+  listImg.style.transform = `translateX(${widthImg * -1 * currentImg}px)`;
+  document.querySelector(".dot-item.active").classList.remove("active");
+  dots[currentImg].classList.add("active");
+};
+let slideShowauto = setInterval(slideShownext, 4000);
 nextHero.addEventListener("click", () => {
   clearInterval(slideShowauto);
-  slideShownext()
-  slideShowauto = setInterval(slideShownext, 4000)
-})
+  slideShownext();
+  slideShowauto = setInterval(slideShownext, 4000);
+});
 prevHero.addEventListener("click", () => {
   clearInterval(slideShowauto);
-  if(currentImg == 0){
+  if (currentImg == 0) {
     currentImg = lengthImgs - 1;
+  } else {
+    currentImg--;
   }
-  else {
-    currentImg--
-  }
-  listImg.style.transform = `translateX(${widthImg * -1 * currentImg}px)`
-  slideShowauto = setInterval(slideShownext, 4000)
-  document.querySelector('.dot-item.active').classList.remove('active')
-  dots[currentImg].classList.add('active')
-})
+  listImg.style.transform = `translateX(${widthImg * -1 * currentImg}px)`;
+  slideShowauto = setInterval(slideShownext, 4000);
+  document.querySelector(".dot-item.active").classList.remove("active");
+  dots[currentImg].classList.add("active");
+});
 // ---------------------------------- Phan trang ---------------------------------
 let thisPage = 1;
 let limit = 8;
@@ -97,10 +95,30 @@ function changePage(i) {
   thisPage = i;
   loadItem();
 }
-// --------------------------------------------- Login -----------------------------------
+// --------------------------------------------- Login Page-----------------------------------
 const homePage = document.querySelector(".home-page");
 const loginPage = document.querySelector(".login-page");
-document.querySelector(".login-btn").addEventListener("click", () => {
-  homePage.style.display = "none";
-  loginPage.style.display = "block";
+const registerPage = document.querySelector(".register-page");
+
+const loginbtn = document.querySelectorAll(".login-btn");
+loginbtn.forEach((login) => {
+  login.addEventListener("click", () => {
+    homePage.style.display = "none";
+    registerPage.style.display = "none";
+    loginPage.style.display = "block";
+    document.getElementById('email-login').focus();
+  });
 });
+// --------------------------------------------- Login funtion-----------------------------------
+
+// --------------------------------------------- Register -----------------------------------
+const registerbtn = document.querySelectorAll(".register-btn");
+registerbtn.forEach((register) => {
+  register.addEventListener("click", () => {
+    homePage.style.display = "none";
+    registerPage.style.display = "block";
+    loginPage.style.display = "none";
+    document.getElementById('name-register').focus();
+  });
+});
+// --------------------------------------------- Register funtion -----------------------------------
